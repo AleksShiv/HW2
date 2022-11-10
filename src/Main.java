830198a2f92b6dd9cbeb130b719b6e714d26e569
Wzooh <sfzp74@gmail.com>
2022-11-10 16:49:23 +0500

    Проверьте ДЗ2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,49 @@
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        var dog = 8;
-        var cat = 3.6;
-        var paper = 763789;
+        double dog = 8;
         System.out.println(dog);
+        var cat = 3.5;
         System.out.println(cat);
+        var paper = 763789;
         System.out.println(paper);
+
         dog = dog + 2;
+        System.out.println(dog);
+        cat = cat + 2;
+        System.out.println(cat);
+        paper = paper + 2;
+        System.out.println(paper);
+
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
+        var friend =19;
+        System.out.println(friend);
+        friend = friend +2;
+        System.out.println(friend);
+        friend = friend/7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog *10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog = frog +4;
+        System.out.println(frog);
+
+        var boxerOne = 78.2;
+        var boxerTwo = 82.7;
+        var boxerBoth = boxerTwo + boxerTwo;
+        System.out.println("Вес обоих бойцов " + boxerBoth);
+        var boxerDiff = boxerTwo - boxerOne;
+        System.out.println("разниц между весами бойцов " + boxerDiff + " кг");
 
 
     }
